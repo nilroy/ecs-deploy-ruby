b6167673d4a9c10dd28b9b3265e3d1a78a6ce54a
Nilanjan Roy <nilanjan.roy@nosto.com>
2018-02-16 14:12:43 +0200

    Added requirements.rb

---
+++ lib/requirements.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
+# stdlib
+require 'logger'
+require 'yaml'
+require 'json'
+require 'pathname'
+require 'base64'
+require 'erb'
+require 'timeout'
+require 'singleton'
+
+# 3rd party
+require 'rubygems'
+require 'aws-sdk-ecs'
+require 'trollop'
+require 'awesome_print'
+require 'pry-byebug'
+
+# internal requires
+require 'AWS/ecs'
